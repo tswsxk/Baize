af327af02fe1e6c8baaa332a52e42acbc15c70dd
tswsxk <tongsw@mail.ustc.edu.cn>
2021-08-04 23:52:50 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -18,4 +18,15 @@ setup(
     ],  # And any other dependencies foo needs
     entry_points={
     },
+    classifiers=[
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        "Environment :: Other Environment",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: Mozilla Public License 2.0 (MPL 2.0)",
+        "Operating System :: OS Independent",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
 )
