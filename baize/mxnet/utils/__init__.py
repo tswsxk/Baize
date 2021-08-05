d583bdf6790eecea577f4f3cbece39a52fbafbb6
Shiwei Tong <tongsw@mail.ustc.edu.cn>
2021-08-05 15:30:51 +0800

    [feat] initialization

---
+++ baize/mxnet/utils/__init__.py
@@ -0,0 +1,6 @@
+# coding: utf-8
+# 2021/8/5 @ tongshiwei
+
+from longling.ML.MxnetHelper import (
+    getF, copy_net, set_embedding, format_sequence, mask_sequence_variable_length, get_begin_state
+)
---
+++ baize/torch/__init__.py
@@ -0,0 +1,4 @@
+# coding: utf-8
+# 2021/8/5 @ tongshiwei
+
+from longling.ML.PytorchHelper import light_module
