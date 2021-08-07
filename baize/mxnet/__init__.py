cbe68df8db0cf44b5da363f4676abb82f29021ea
Shiwei Tong <tongsw@mail.ustc.edu.cn>
2021-08-07 17:20:06 +0800

    [feat] initialization

--- baize/mxnet/__init__.py
+++ baize/mxnet/__init__.py
@@ -1,4 +1,6 @@
 # coding: utf-8
 # 2021/8/5 @ tongshiwei
 
-from longling.ML.MxnetHelper import light_module
+from longling.ML.MxnetHelper import light_module, Configuration, ConfigurationParser
+from longling.ML.MxnetHelper import fit_wrapper, loss_dict2tmt_mx_loss
+from longling.ML.PytorchHelper import save_params, load_net
--- baize/torch/__init__.py
+++ baize/torch/__init__.py
@@ -1,5 +1,6 @@
 # coding: utf-8
 # 2021/8/5 @ tongshiwei
 
-from longling.ML.PytorchHelper import light_module, Configuration
+from longling.ML.PytorchHelper import light_module, Configuration, ConfigurationParser
 from longling.ML.PytorchHelper import fit_wrapper, loss_dict2tmt_torch_loss, eval_wrapper
+from longling.ML.PytorchHelper import save_params, load_net
