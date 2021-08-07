98160cbae89d638452e97fab3d0bacf5d5244472
Shiwei Tong <tongsw@mail.ustc.edu.cn>
2021-08-07 15:55:56 +0800

    [chore] update dependencies

--- setup.py
+++ setup.py
@@ -1,3 +1,4 @@
+import logging
 from setuptools import setup, find_packages
 
 test_deps = [
@@ -6,15 +7,45 @@ test_deps = [
     'pytest-flake8'
 ]
 
+try:
+    import mxnet
+
+    mxnet_requires = []
+except ModuleNotFoundError:
+    mxnet_requires = ["mxnet"]
+except Exception as e:
+    mxnet_requires = []
+    logging.error(e)
+
+try:
+    import torch
+
+    ml_pytorch_deps = []
+except ModuleNotFoundError:
+    import sys
+
+    if 5 <= sys.version_info[1]:
+        ml_pytorch_deps = ["torch"]
+    else:
+        ml_pytorch_deps = []
+        logging.warning("Current python version %s is not supported by pytorch", str(sys.version_info[:2]))
+except Exception as e:
+    ml_pytorch_deps = []
+    logging.error(e)
+
 setup(
     name='PyBaize',
     version='0.0.2',
     extras_require={
-        'test': test_deps,
+        'test': test_deps + mxnet_requires + ml_pytorch_deps,
+        'mxnet': mxnet_requires,
+        'torch': ml_pytorch_deps,
+        'full': mxnet_requires + ml_pytorch_deps
     },
     packages=find_packages(),
     install_requires=[
-        'longling>=1.3.29',
+        'longling[ml]>=1.3.29',
+        'nni>=1.8'
     ],  # And any other dependencies foo needs
     entry_points={
     },
