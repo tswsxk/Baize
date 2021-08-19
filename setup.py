d386d936ceed37a561784f61f46b8bd3c2038453
Shiwei Tong <tongsw@mail.ustc.edu.cn>
2021-08-19 11:26:01 +0800

    [version] 0.0.4 [chore] update dependency

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ except Exception as e:
 
 setup(
     name='PyBaize',
-    version='0.0.3',
+    version='0.0.4',
     extras_require={
         'test': test_deps + mxnet_requires + ml_pytorch_deps,
         'mxnet': mxnet_requires,
@@ -44,7 +44,7 @@ setup(
     },
     packages=find_packages(),
     install_requires=[
-        'longling[ml]>=1.3.29',
+        'longling[ml]>=1.3.30',
         'nni>=1.8'
     ],  # And any other dependencies foo needs
     entry_points={
