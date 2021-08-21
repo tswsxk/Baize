036937f31246d1b75e94fb56449aa4bdc7743d97
Shiwei Tong <tongsw@mail.ustc.edu.cn>
2021-08-21 21:47:47 +0800

    [feat] add get_params_filepath

--- baize/__init__.py
+++ baize/__init__.py
@@ -2,5 +2,5 @@ from longling import json_load, path_append, as_out_io, as_io
 from longling import config_logging, iterwrap
 from longling import as_list
 from longling import Timer
-from longling.ML import get_epoch_params_filepath
+from longling.ML import get_epoch_params_filepath, get_params_filepath
 from longling.ML.toolkit.analyser import get_best
