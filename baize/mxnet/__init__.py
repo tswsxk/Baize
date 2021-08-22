79e8162f62b2037ef3b0fe7c8635209007a08ee3
Shiwei Tong <tongsw@mail.ustc.edu.cn>
2021-08-22 20:21:07 +0800

    [fix] wrong export [feat] export split_and_load

--- baize/mxnet/__init__.py
+++ baize/mxnet/__init__.py
@@ -3,4 +3,5 @@
 
 from longling.ML.MxnetHelper import light_module, Configuration, ConfigurationParser
 from longling.ML.MxnetHelper import fit_wrapper, loss_dict2tmt_mx_loss
-from longling.ML.PytorchHelper import save_params, load_net
+from longling.ML.MxnetHelper import save_params, load_net
+from longling.ML.MxnetHelper import split_and_load
